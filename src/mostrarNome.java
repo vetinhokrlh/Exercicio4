5532870115466a64196a2307140d9d4426306a89
victor <victor.silva.ramos@escola.pr.gov.br>
2024-09-30 11:59:50 -0300

    iniciando os arquivos

---
+++ src/mostrarNome.java
@@ -0,0 +1,9 @@
+import java.nio.file.FileSystemNotFoundException;
+import java.util.Scanner;
+
+public class mostrarNome {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Qual seu nome");
+        String nome
+    }}
